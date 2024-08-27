42d144caf2b0b2976c7c33ceab5fbe07aecce886
gap <zh>
2024-08-27 09:33:35 +0800

    first commit

---
+++ personalLoan/business/src/main/java/com/xzllcb/business/BusinessApplication.java
@@ -0,0 +1,20 @@
+package com.xzllcb.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author：lixin
+ * @Package：com.xzllcb.approval
+ * @Project：bank
+ * @name：ApprovalApplication
+ * @Date：2024/8/26 16:41
+ * @Filename：ApprovalApplication
+ * @Description：
+ */
+@SpringBootApplication
+public class BusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class);
+    }
+}
---
+++ personalLoan/quota/src/main/java/com/xzllcb/quota/QuotaApplication.java
@@ -0,0 +1,20 @@
+package com.xzllcb.quota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author：lixin
+ * @Package：com.xzllcb.approval
+ * @Project：bank
+ * @name：ApprovalApplication
+ * @Date：2024/8/26 16:41
+ * @Filename：ApprovalApplication
+ * @Description：
+ */
+@SpringBootApplication
+public class QuotaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuotaApplication.class);
+    }
+}
